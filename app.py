e098c33304138f5b555681a9820e525e122e538d
glody007 <glodymbutwile@gmail.com>
2022-07-27 10:22:17 +0200

    feat: Add venue with added fiels.
    website_link, seeking_talent and seeking_description.

--- app.py
+++ app.py
@@ -224,8 +224,8 @@ def create_venue_form():
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
   form = VenueForm()
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+  # insert form data as a new Venue record in the db, instead
+  # modify data to be the data object returned from db insertion
   if form.validate_on_submit():
     try:
       venue = Venue(
@@ -236,20 +236,25 @@ def create_venue_submission():
         genres=form.genres.data,
         phone=form.phone.data,
         image_link=form.image_link.data,
-        facebook_link=form.facebook_link.data
+        facebook_link=form.facebook_link.data,
+        website_link=form.website_link.data,
+        seeking_talent=form.seeking_talent.data,
+        seeking_description=form.seeking_description.data
       )
       db.session.add(venue)
-      db.session.commit()
+      data = db.session.commit()
       # on successful db insert, flash success
-      flash('Venue ' + form.name.data + ' was successfully listed!')
+      flash('Venue ' + data.name + ' was successfully listed!')
     except:
       db.session.rollback()
       error=True
       print(sys.exc_info())
-      # TODO: on unsuccessful db insert, flash an error instead.
+      # on unsuccessful db insert, flash an error instead.
       flash('An error occurred. Venue ' + form.name.data + ' could not be listed.')
     finally:
       db.session.close()
+  else: 
+    flash('Your input is not valide. fill required fields with good type')
   return render_template('pages/home.html')
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
