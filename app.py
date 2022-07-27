2919034e40a9a899d3791507c82d598f97512c4f
glody007 <glodymbutwile@gmail.com>
2022-07-27 17:58:28 +0200

    feat: Add relation Artist and show.

--- app.py
+++ app.py
@@ -60,11 +60,13 @@ class Artist(db.Model):
     website_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String(500))
+    shows = db.relationship('Show', backref='artist', lazy=True) 
 
 class Show(db.Model):
   __tablename__ = 'Show'
   id = db.Column(db.Integer, primary_key=True)
   venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
 
 #----------------------------------------------------------------------------#
 # Filters.
