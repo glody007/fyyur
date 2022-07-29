ce17b45abb9073db46324fc113e64ef13d303d79
glody007 <glodymbutwile@gmail.com>
2022-07-29 14:49:22 +0200

    feat: Artist phone validation.

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField
-from wtforms.validators import DataRequired, AnyOf, URL
+from wtforms.validators import DataRequired, AnyOf, URL, Length, Regexp
 
 class ShowForm(Form):
     artist_id = StringField(
@@ -192,8 +192,11 @@ class ArtistForm(Form):
         ]
     )
     phone = StringField(
-        # TODO implement validation logic for phone 
-        'phone'
+        # implement validation logic for phone 
+        'phone', validators=[
+            Length(min=10),
+            Regexp(regex='^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$')
+        ]
     )
     image_link = StringField(
         'image_link'
